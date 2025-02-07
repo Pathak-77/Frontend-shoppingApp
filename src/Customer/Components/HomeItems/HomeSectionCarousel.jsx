9d99e3ace9621f0db70f3aab1ce79ae4ba9d433b
Pathak-77/ <aayushpathak77@gmail.com>
2025-02-07 21:15:43 +0545

    added products

--- src/Customer/Components/HomeItems/HomeSectionCarousel.jsx
+++ src/Customer/Components/HomeItems/HomeSectionCarousel.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
-import 'react-alice-carousel/lib/alice-carousel.css'
-import Cards from './ItemCards'
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from "@heroicons/react/24/outline";
+import Cards from "./ItemCards";
 
 const responsive = {
-    0: { items: 1 },
-    568: { items: 2 },
-    1024: { items: 3 },
+  0: { items: 2 },
+  568: { items: 3 },
+  1024: { items: 5.5 },
 };
 
-const CardCarousel = ()=> {
-    const items = [1,1,1,1].map((item) => { return <Cards />})
-    
-    return(
-        <div>
-        <AliceCarousel
-            mouseTracking
-            items={items}
-            paddingLeft={5}
-            paddingRight={5}
-            disableButtonsControls
-            disableDotsControls
-            responsive={responsive}
+const CardCarousel = () => {
+  const items = [1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => {
+    return <Cards />;
+  });
+
+  return (
+    <div className="relative border m-3">
+      <div className="relative border  p-5">
+        {/* Left Arrow Icon Positioned on Top */}
+        <ArrowLeftCircleIcon
+          className="absolute top-1/2 left-2 transform -translate-y-1/2 -translate-x-5 z-10 cursor-pointer"
+          height={40}
+          fill="black"
+          color="white"
         />
-        </div>
-    );
-}
+
+        <ArrowRightCircleIcon
+          className="absolute top-1/2 right-2 transform -translate-y-1/2 -translate-x-[-5] z-10 cursor-pointer"
+          height={40}
+          fill="black"
+          color="white"
+        />
+
+        {/* scannable item carousel*/}
+        <h3 className="bold text-size-[22px]">Mens shoes</h3>
+        <AliceCarousel
+          mouseTracking
+          
+          items={items}
+          paddingLeft={5}
+          paddingRight={5}
+          disableButtonsControls
+          disableDotsControls
+          responsive={responsive}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default CardCarousel;
--- src/Customer/Components/HomeItems/ItemCards.jsx
+++ src/Customer/Components/HomeItems/ItemCards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Cards = ()=> {
     
     return(
-            <section className='h-[250px] w-[200px] mt-1 p-1 border-4 border-indigo-500 rounded-xl flex-wrap'>
+            <section className='h-[250px] w-[200px] mt-1 p-1 border-4 border-indigo-500 rounded-xl flex-wrap relative'>
                  <div className='mb-2 h-[120px] overflow-hidden'>
                     <img src='https://media.gucci.com/content/PromoComponent_Standard_632x395/1737653435/PromoComponent_Gucci-VDAY-Jan25-GUCCI-VDAY-MARSHALL-08-035_001_Default.jpg'
                     alt='' />
